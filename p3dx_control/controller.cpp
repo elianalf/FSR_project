cf207488c37f5afe3e2b185b75bdbb40b49d8d49
Eliana <lafrazia.eliana@gmail.com>
2020-09-21 22:48:38 +0200

    8commit

--- p3dx_control/controller.cpp
+++ p3dx_control/controller.cpp
@@ -4,7 +4,9 @@
 #include <tf/tf.h>
 #include "nav_msgs/Odometry.h"
 #include "boost/thread.hpp"
-#include "Eigen/Dense"
+#include <eigen3/Eigen/Geometry>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Core>
 #include "nav_msgs/Path.h"
 
 using namespace std;
@@ -12,6 +14,7 @@ using namespace std;
 #define pi 3.14
 #define Ts 0.01
 #define lin_vel_max 0.6
+#define ang_vel_max 0.3
 #define nNodesPath 2
 double x_start=0;
 double y_start=0;
@@ -61,13 +64,11 @@ void MOVER::odom_cb(nav_msgs::OdometryConstPtr odom_ptr) {
 
 void MOVER::nav_loop(){
    cout<<"5"<<endl;
-   double dx=0;
-  double dy=0;
-  double ro=0;
-  double gamma=0;
-  double delta=0;
+ 
   double teta=0;
    geometry_msgs::Twist t;
+   geometry_msgs::Twist v;
+   geometry_msgs::Twist cmd_v;
    t = geometry_msgs::Twist();
    Eigen::Vector2d curr_p;
    ros::Rate r(100); //10 milliseconds
@@ -82,67 +83,141 @@ void MOVER::nav_loop(){
    vector<double> teta_des;
    double xdd;
    double ydd;
-   int n_step[nNodesPath];
+   int n_step;
+   double teta_new=0;
+   int nstep=0;
+   double teta_old=0;
+   double teta_e=0;
    double interval;
    x_goal[0]=1;
    y_goal[0]=1;
    x_goal[1]=0;
    y_goal[1]=2;
-   cout<<"n "<<n_step[0]<<endl;
-   int i=1;
+   
+   //initialization
    c_p.pose.position.x =x_start;
    c_p.pose.position.y = y_start;
    p.poses.push_back(c_p);
    t.linear.x=0;
    t.linear.y=0;
+  
    pd.push_back(t);
    v_des.push_back(t);
    teta_des.push_back(0);
-   
+   double teta_buf=0;
+   //plan trajectory
+   int i=1;
    for(int j=0;j<nNodesPath;j++){
       des_node(0)=x_goal[j];
        des_node(1)=y_goal[j];
        dir = des_node - curr_p;
+       teta_old=teta_new;
+       teta_new=atan2(dir(1),dir(0));
        interval=dir.norm() / lin_vel_max; //time interval
-       n_step[j]=interval/Ts;//n step
+       n_step=interval/Ts;//n step
+       
+       //xdd=(pd[i].linear.x-pd[i-1].linear.x)/Ts;
+       //ydd=(pd[i].linear.y-pd[i-1].linear.y)/Ts;
+       //cout<< "xdd ydd "<<xdd<<" "<<ydd<<endl;
+       teta_e=teta_new-teta_old;
+       if(fabs((teta_new+3.14) - (teta_old+3.14)) > 3.14){ v.angular.z = ang_vel_max * ((teta_e>0)?-1:1); }
+       else { v.angular.z = ang_vel_max * ((teta_e>0)?1:-1); }
+          
+       interval=abs(teta_e) / ang_vel_max; //time interval
+       nstep=interval/Ts;
+       v.linear.x=0;
+       cout<< "v w_ "<<v.linear.x<<" "<<v.angular.z<<endl;
+       //teta_buf=teta_old;
+       for(int k=0;k<nstep;k++){
+         //teta_buf+= teta_buf
+         teta_des.push_back(teta_new);
+         p.poses.push_back(c_p);
+         pd.push_back(t);
+         v_des.push_back(v);
+         i++;
+       }
+       cout<<"size "<<v_des.size()<<" "<<p.poses.size()<<endl;
      while((des_node - curr_p).norm() > 0.05){
        
        dir = des_node - curr_p;
        dir = dir / dir.norm();
-       curr_p+= dir*(1.0/n_step[j]); //4.46 pag 185
+       curr_p+= dir*(1.0/n_step); //4.46 pag 185
        //cout<< "Current position "<< curr_p.transpose()<<endl;
        c_p.pose.position.x = curr_p(0);
        c_p.pose.position.y = curr_p(1);
        p.poses.push_back(c_p);
-       cout<<i<<" P "<<p.poses[i].pose.position.x<<" "<<p.poses[i-1].pose.position.x<<endl;
+       //cout<<i<<" P "<<p.poses[i].pose.position.x<<" "<<p.poses[i-1].pose.position.x<<endl;
+       
        
        t.linear.x=(p.poses[i].pose.position.x-p.poses[i-1].pose.position.x)/Ts;
        t.linear.y=(p.poses[i].pose.position.y-p.poses[i-1].pose.position.y)/Ts;
-       cout<< "vel "<<t.linear.x<<" "<<t.linear.y<<endl;
+       //cout<< "vel "<<t.linear.x<<" "<<t.linear.y<<endl;
        pd.push_back(t);
        
-       xdd=(pd[i].linear.x-pd[i-1].linear.x)/Ts;
-      ydd=(pd[i].linear.y-pd[i-1].linear.y)/Ts;
-     
-       t.linear.x=sqrt(pd[i].linear.x*pd[i].linear.x+pd[i].linear.y*pd[i].linear.y); //v des
-       t.angular.z=(ydd*pd[i].linear.x-xdd*pd[i].linear.y)/(pd[i].linear.x*pd[i].linear.x+pd[i].linear.y*pd[i].linear.y); //w des
-       v_des.push_back(t);
-       cout<< "v w "<<t.linear.x<<" "<<t.angular.z<<endl;
-       double teta_buf=atan2(pd[i].linear.y,pd[i].linear.x);
-       teta_des.push_back(teta_buf);
-         cout<<"teta "<<teta_des[i]<<endl;
+       
+       //teta_buf=atan2(pd[i].linear.y,pd[i].linear.x);
+       teta_des.push_back(teta_new);
+       //cout<<"teta "<<teta_des[i]<<endl;
+       //teta_buf=teta_des[i]-teta_des[i-1];
+       v.angular.z = 0;
+       v.linear.x=sqrt(pd[i].linear.x*pd[i].linear.x+pd[i].linear.y*pd[i].linear.y); //v des
+       v_des.push_back(v);
+       //cout<< "v_ w "<<v.linear.x<<" "<<v.angular.z<<endl;
+       //t.angular.z=(ydd*pd[i].linear.x-xdd*pd[i].linear.y)/(pd[i].linear.x*pd[i].linear.x+pd[i].linear.y*pd[i].linear.y); //w des
+       
+    
        i++;
      }
+     curr_p+= dir*(1.0/n_step);
    }
    
-   
-   
-   t.linear.x=0;
-   t.angular.z=0;
-   twist_pub.publish(t);
-   r.sleep();
-    
+   i=0;
+   int path_size=v_des.size();
   
+   
+   Eigen::Matrix3d cos_sin;
+   Eigen::Vector3d q_e;
+   Eigen::Vector3d e;
+   double u1=0;
+   double u2=0;
+   int k1=1.4;
+   int k2=0;
+   int k3=1.4;
+   
+   for(i=0;i<path_size;i++){
+   teta=_yaw;
+   cos_sin(0,0)=cos(teta);
+   cos_sin(0,1)=sin(teta);
+   cos_sin(0,2)=0;
+   cos_sin(1,0)=-sin(teta);
+   cos_sin(1,1)=cos(teta);
+   cos_sin(1,2)=0;
+   cos_sin(2,0)=0;
+   cos_sin(2,1)=0;
+   cos_sin(2,2)=1;
+   e<< (p.poses[i].pose.position.x -odom_pos.x), (p.poses[i].pose.position.y-odom_pos.y), (teta_des[i]-teta);
+   e=cos_sin*q_e;
+   if(v_des[i].linear.x==0){
+      cmd_v.linear.x=0;
+   
+   }
+   else{
+      k2=(1-v_des[i].angular.z*v_des[i].angular.z)/(v_des[i].linear.x*v_des[i].linear.x);
+      u1=-k1*e(0);
+      u2=-k2*e(1)-k3*e(2);
+      cmd_v.linear.x=v_des[i].linear.x*cos(e(2)) - u1;
+   
+   }
+   cmd_v.angular.z=v_des[i].angular.z - u2;
+   
+   
+   twist_pub.publish(cmd_v);
+   r.sleep();
+   } 
+  cmd_v.angular.z=0;
+  cmd_v.linear.x=0; 
+   twist_pub.publish(cmd_v);
+   
   while (ros::ok()){r.sleep(); }
 }
 
